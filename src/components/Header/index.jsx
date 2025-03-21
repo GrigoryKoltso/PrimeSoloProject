32bfeb631b6af39d7167c156fea6f6d17dc242ba
Grigory <grifao26@gmail.com>
2025-03-21 18:51:39 +0100

    burger-m and a bit of adaptability

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,6 +8,7 @@ import lightMode from '../../assets/Buttons/lightMode.svg'
 import Navbar from '../Navbar';
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux';
+import { useState } from 'react'
 const Header = () => {
 
     const cartState = useSelector(store => store.cart);
@@ -15,6 +16,12 @@ const Header = () => {
 
     const favoriteState = useSelector(store => store.favorite);
     const favoriteCount = favoriteState.length
+
+    const [burgerActive, setBurgerActive] = useState(false); // состояние для бургера
+
+    const handleBurgerClick = () => {
+        setBurgerActive((prev) => !prev); // переключаем состояние бургера
+    };
 
     return (
         <header className={`${s.header} app-container`}>
@@ -26,6 +33,24 @@ const Header = () => {
             </div>
 
             <Navbar />
+
+            <div className={`${s.navList} ${burgerActive ? s.active : ""}`}>
+                <div className={s.header__menu}>
+                    <Link to="/">Main Page</Link>
+                    <Link to="/categories">Categories</Link>
+                    <Link to="/products">All products</Link>
+                    <Link to="/sales">All sales</Link>
+                </div>
+            </div>
+
+            <div
+                className={`${s.header__burger} ${burgerActive ? s.active : ""}`}
+                onClick={handleBurgerClick}
+            >
+                <span className={s.line}></span>
+                <span className={s.line}></span>
+                <span className={s.line}></span>
+            </div>
 
             <div className={s.header__rightIcons}>
                 <Link to='/favorites'>
